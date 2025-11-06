203b9ceae672d3d16613fc7be18ec2bf9552330a
Kiran Chaudhary <kiranchaudhary.me0610@gmail.com>
2025-11-06 19:52:48 +0530

    chore: create django project and core apps

--- event_booking/settings.py
+++ event_booking/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "drf_yasg",
+    "corsheaders",
+    "channels",
+    "accounts",
+    "events",
+    "bookings",
+    
 ]
 
 MIDDLEWARE = [
@@ -120,3 +129,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+ASGI_APPLICATION = "event_booking.asgi.application"
